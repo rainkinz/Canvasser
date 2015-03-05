9dd9f50cde0153cfea505909c85076653e0af0a5
rainkinz <brendan.grainger@gmail.com>
2015-03-05 08:39:03 -0500

    Intial Version

---
+++ Canvasser/AddressTVC.swift
@@ -0,0 +1,41 @@
+//
+//  AddressTVC.swift
+//  Canvasser
+//
+//  Created by brendan on 3/4/15.
+//  Copyright (c) 2015 SwiftMeetup. All rights reserved.
+//
+
+import UIKit
+
+class AddressTVC: UITableViewController
+{
+    var addressList = [String]()
+    
+    override func viewDidLoad() {
+        addressList.append("Brendan's House")
+        addressList.append("Cameron's House")
+        addressList.append("Alex's House")
+        addressList.append("Bob's House")
+        addressList.append("Jane's House")
+        addressList.append("John's House")
+    }
+        
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return addressList.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "addressCell")
+        cell.textLabel?.text = addressList[indexPath.row]
+        println(cell.textLabel?.text)
+        return cell
+    }
+    
+}
+
+
